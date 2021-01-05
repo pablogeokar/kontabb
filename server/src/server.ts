42dd7dbeca16468cdb4c3299e0e0f56836cd73d6
Pablo George <pablogeokar@hotmail.com>
2021-01-05 10:40:39 -0300

    PREPARANDO O LAYOUT DO DASHBOARD

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,5 +24,6 @@ app.use(limiter)
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/assets', express.static(path.resolve(__dirname, '..', 'assets')))
 
 app.listen(3333)
